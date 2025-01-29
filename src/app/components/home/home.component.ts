89c712722d48d8329dd3f2450c832996ef35ec23
Gustavo Stolze <gustavostolzedeandrade@gmail.com>
2025-01-29 01:09:21 -0300

    home / game-item

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,10 +2,12 @@ import { Component, inject } from '@angular/core';
 import { GameService } from '../../services/game.service';
 import { GameMinI } from '../../interfaces/gameMin.interface';
 import { CommonModule } from '@angular/common';
+import { GameItemComponent } from '../game-item/game-item.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, GameItemComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

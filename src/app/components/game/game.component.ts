b0370f6a3b2f4e854eb944b7a94f07077372b9fd
Gustavo Stolze <gustavostolzedeandrade@gmail.com>
2025-01-29 21:50:29 -0300

    game component

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { GameI } from '../../interfaces/game.interface';
+import { GameService } from '../../services/game.service';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit, OnDestroy {
+  game: GameI | undefined;
+  subscriptions: Subscription | undefined;
 
+  constructor (
+    private gameService: GameService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+      let id = this.route.snapshot.paramMap.get('id');
+      this.subscriptions = this.gameService.getGames(Number(id)).subscribe((game) => {
+      this.game = game;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions?.unsubscribe();
+  }
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GameMinI } from '../interfaces/gameMin.interface';
 import { Observable } from 'rxjs';
+import { GameI } from '../interfaces/game.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +14,9 @@ export class GameService {
 
   public getMinGames(): Observable<GameMinI[]> {
     return this.http.get<GameMinI[]>(`${this.urlApi}/games`);
+  }
+
+  public getGames(id: number): Observable<GameI> {
+    return this.http.get<GameI>(`${this.urlApi}/games/${id}`);
   }
 }
